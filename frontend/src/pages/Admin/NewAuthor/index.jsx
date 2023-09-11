2c46789966324f6dd672efd26b27350bff902fb4
Your Name <your@email.com>
2023-09-11 20:40:53 +0200

    Add newAuthor-reducer

--- frontend/src/pages/Admin/NewAuthor/index.jsx
+++ frontend/src/pages/Admin/NewAuthor/index.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import axios from "axios";
 import useAxios from "../../../hooks/useAxios";
 import style from "./newAuthor.module.scss";
+import newAuthorReducer, {
+  initialState,
+  INPUT_KNOWN_NAME,
+  INPUT_FIRST_NAME,
+  INPUT_LAST_NAME,
+  INPUT_PERIOD_ID,
+  INPUT_PHILO_CURRENT,
+  INPUT_BORN_DATE,
+  INPUT_DEAD_DATE,
+  INPUT_ERA,
+} from "./utils/newAuthor-reducer";
 
 export default function NewAuthor() {
-  const [knownName, setKnownName] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [periodId, setPeriodId] = useState(null);
-  const [philoCurrent, setPhiloCurrent] = useState(null);
-  const [bornDate, setBornDate] = useState("");
-  const [deadDate, setDeadDate] = useState("");
-  const [era, setEra] = useState("");
+  const [newAuthor, setNewAuthor] = useReducer(newAuthorReducer, initialState);
 
   const periodsResponse = useAxios({
     method: "get",
@@ -20,25 +24,18 @@ export default function NewAuthor() {
 
   const handleNewAuthorPost = () => {
     if (
-      knownName &&
-      firstName &&
-      lastName &&
-      periodId &&
-      philoCurrent &&
-      bornDate &&
-      deadDate &&
-      era
+      newAuthor.knownName &&
+      newAuthor.firstName &&
+      newAuthor.lastName &&
+      newAuthor.periodId &&
+      newAuthor.philoCurrent &&
+      newAuthor.bornDate &&
+      newAuthor.deadDate &&
+      newAuthor.era
     ) {
       axios
         .post(`${import.meta.env.VITE_BACKEND_URL}/api/authors`, {
-          knownName,
-          firstName,
-          lastName,
-          periodId,
-          philoCurrent,
-          bornDate,
-          deadDate,
-          era,
+          ...newAuthor,
         })
         .then((response) => console.info(response))
         .catch((err) => console.error(err));
@@ -61,8 +58,13 @@ export default function NewAuthor() {
             type="text"
             name="knownName"
             className={style.input}
-            value={knownName}
-            onChange={(e) => setKnownName(e.target.value)}
+            value={newAuthor.knownName}
+            onChange={(e) =>
+              setNewAuthor({
+                type: INPUT_KNOWN_NAME,
+                payload: { knownName: e.target.value },
+              })
+            }
           />
         </label>
         <label htmlFor="firstName" className={style.inputLabel}>
@@ -71,8 +73,13 @@ export default function NewAuthor() {
             type="text"
             name="firstName"
             className={style.input}
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
+            value={newAuthor.firstName}
+            onChange={(e) =>
+              setNewAuthor({
+                type: INPUT_FIRST_NAME,
+                payload: { firstName: e.target.value },
+              })
+            }
           />
         </label>
         <label htmlFor="lastName" className={style.inputLabel}>
@@ -81,7 +88,13 @@ export default function NewAuthor() {
             type="text"
             name="lastName"
             className={style.input}
-            onChange={(e) => setLastName(e.target.value)}
+            value={newAuthor.lastName}
+            onChange={(e) =>
+              setNewAuthor({
+                type: INPUT_LAST_NAME,
+                payload: { lastName: e.target.value },
+              })
+            }
           />
         </label>
         <label htmlFor="periodId" className={style.inputLabel}>
@@ -90,7 +103,12 @@ export default function NewAuthor() {
             name="periodId"
             id=""
             className={style.select}
-            onChange={(e) => setPeriodId(e.target.value)}
+            onChange={(e) =>
+              setNewAuthor({
+                type: INPUT_PERIOD_ID,
+                payload: { periodId: e.target.value },
+              })
+            }
           >
             <option defaultChecked>Select the periodId</option>
             {periodsResponse &&
@@ -107,7 +125,12 @@ export default function NewAuthor() {
             name="era"
             id=""
             className={style.select}
-            onChange={(e) => setPhiloCurrent(e.target.value)}
+            onChange={(e) =>
+              setNewAuthor({
+                type: INPUT_PHILO_CURRENT,
+                payload: { philoCurrent: e.target.value },
+              })
+            }
           >
             <option defaultChecked>Select the periodId</option>
             <option value="1">Socratic</option>
@@ -121,8 +144,13 @@ export default function NewAuthor() {
               type="date"
               name="bornDate"
               className={style.inputDates}
-              value={bornDate}
-              onChange={(e) => setBornDate(e.target.value)}
+              value={newAuthor.bornDate}
+              onChange={(e) =>
+                setNewAuthor({
+                  type: INPUT_BORN_DATE,
+                  payload: { bornDate: e.target.value },
+                })
+              }
             />
           </label>
           <label htmlFor="deadDate" className={style.inputLabelDead}>
@@ -130,9 +158,14 @@ export default function NewAuthor() {
             <input
               type="date"
               name="deadDate"
-              value={deadDate}
+              value={newAuthor.deadDate}
               className={style.inputDates}
-              onChange={(e) => setDeadDate(e.target.value)}
+              onChange={(e) =>
+                setNewAuthor({
+                  type: INPUT_DEAD_DATE,
+                  payload: { deadDate: e.target.value },
+                })
+              }
             />
           </label>
           <label htmlFor="era" className={style.inputLabelEra}>
@@ -141,7 +174,12 @@ export default function NewAuthor() {
               name="era"
               id=""
               className={style.selectEra}
-              onChange={(e) => setEra(e.target.value)}
+              onChange={(e) =>
+                setNewAuthor({
+                  type: INPUT_ERA,
+                  payload: { era: e.target.value },
+                })
+              }
             >
               <option value="BCE">BCE</option>
               <option value="CE">CE</option>
---
+++ frontend/src/pages/Admin/NewAuthor/utils/newAuthor-reducer.js
@@ -0,0 +1,90 @@
+export const INPUT_KNOWN_NAME = "inputKnownName";
+export const INPUT_FIRST_NAME = "inputFirstName";
+export const INPUT_LAST_NAME = "inputLastName";
+export const INPUT_PERIOD_ID = "inputPeriodId";
+export const INPUT_PHILO_CURRENT = "inputPhiloCurrent";
+export const INPUT_BORN_DATE = "inputBornDate";
+export const INPUT_DEAD_DATE = "inputDeadDate";
+export const INPUT_ERA = "imputEra";
+
+export const initialState = {
+  knownName: "",
+  firstName: "",
+  lastName: "",
+  periodId: null,
+  philoCurrent: null,
+  bornDate: "",
+  deadDate: "",
+  era: "",
+};
+
+export default function newAuthorReducer(state, action) {
+  switch (action.type) {
+    case INPUT_KNOWN_NAME: {
+      const { knownName } = action.payload;
+      return {
+        ...state,
+        knownName,
+      };
+    }
+
+    case INPUT_FIRST_NAME: {
+      const { firstName } = action.payload;
+      return {
+        ...state,
+        firstName,
+      };
+    }
+
+    case INPUT_LAST_NAME: {
+      const { lastName } = action.payload;
+      return {
+        ...state,
+        lastName,
+      };
+    }
+
+    case INPUT_PERIOD_ID: {
+      const { periodId } = action.payload;
+      return {
+        ...state,
+        periodId,
+      };
+    }
+
+    case INPUT_PHILO_CURRENT: {
+      const { philoCurrent } = action.payload;
+      return {
+        ...state,
+        philoCurrent,
+      };
+    }
+
+    case INPUT_BORN_DATE: {
+      const { bornDate } = action.payload;
+      return {
+        ...state,
+        bornDate,
+      };
+    }
+
+    case INPUT_DEAD_DATE: {
+      const { deadDate } = action.payload;
+      return {
+        ...state,
+        deadDate,
+      };
+    }
+
+    case INPUT_ERA: {
+      const { era } = action.payload;
+      return {
+        ...state,
+        era,
+      };
+    }
+
+    default:
+      return state;
+  }
+}
