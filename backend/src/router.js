fd728b2f29f40aff10bc6628252c5fb6e594059b
Your Name <your@email.com>
2023-09-22 16:07:39 +0200

    Add verifyToken

--- backend/src/router.js
+++ backend/src/router.js
@@ -13,17 +13,26 @@ const authorControllers = require("./controllers/authorControllers");
 const eventPhraseControllers = require("./controllers/eventPhraseControllers");
 const periodControllers = require("./controllers/periodControllers");
 
-const { hashPassword, verifyPassword } = require("./services/checkAuth");
+const {
+  hashPassword,
+  verifyPassword,
+  verifyToken,
+} = require("./services/checkAuth");
 
+//* OPEN ROUTES
+router.post("/login", userControllers.readByEmail, verifyPassword);
 router.post("/avatar", upload.single("avatar"), uploadAvatar.postAvatar);
+router.post("/users", hashPassword, userControllers.add);
+router.get("/phrases5", phraseControllers.browse5);
 
+router.use(verifyToken);
+//* PROTECTED ROUTES
+//* Users
 router.get("/users", userControllers.browse);
 router.get("/users/:id", userControllers.read);
-router.post("/login", userControllers.readByEmail, verifyPassword);
-router.post("/users", hashPassword, userControllers.add);
 
+//* Phrases
 router.get("/phrases", phraseControllers.browse);
-router.get("/phrases5", phraseControllers.browse5);
 router.get("/phrases/:id", phraseControllers.read);
 router.get("/phrases4/events/:id", phraseControllers.read4ByEventId);
 router.get("/phrases4/randomevents", phraseControllers.read4ByRandomEvent);
@@ -31,12 +40,12 @@ router.post("/phrases", phraseControllers.add);
 router.put("/phrases/:id", phraseControllers.edit);
 router.delete("/phrases/:id", phraseControllers.destroy);
 
+//* Events
 router.get("/events", eventControllers.browse);
 router.get("/events/categories/:id", eventControllers.browseAllByCategoryId);
 router.get("/events/:title", eventControllers.browseAllByTitle);
 router.get("/events/:id", eventControllers.read);
 router.post("/events", eventControllers.add);
-
 router.get("/eventphrase", eventPhraseControllers.browse);
 router.get("/eventphrase/:id", eventPhraseControllers.browseByPhraseId);
 router.post("/eventphrase", eventPhraseControllers.add);
@@ -45,14 +54,17 @@ router.delete(
   eventPhraseControllers.destroy
 );
 
+//* Categories
 router.get("/categories", categoryControllers.browse);
 router.get("/categories/:id", categoryControllers.read);
 router.post("/categories", categoryControllers.add);
 
+//* Authors
 router.get("/authors", authorControllers.browse);
 router.get("/authors/:id", authorControllers.read);
 router.post("/authors", authorControllers.add);
 
+//* Periods
 router.get("/periods", periodControllers.browse);
 router.get("/periods/:id", periodControllers.read);
 router.post("/periods", periodControllers.add);
