949028bbf637cde37e0fda5c244d75349f782219
Your Name <your@email.com>
2023-10-28 09:59:09 +0200

    Resolved table style problem

--- frontend/src/pages/Admin/TablesDB/index.jsx
+++ frontend/src/pages/Admin/TablesDB/index.jsx
@@ -1,24 +1,43 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import useAxios from "../../../hooks/useAxios";
 
 import style from "./tablesDB.module.scss";
+import ModifyPhrase from "../../../components/ModifyPhrase";
 
 export default function TablesDB() {
   const { table } = useParams();
 
-  const tableResponse = useAxios({
-    method: "get",
-    endpoint: `${table}`,
-  });
+  const [selectedPhraseId, setSelectedPhraseId] = useState("");
+  const [modifyPhrase, setModifyPhrase] = useState(false);
+  const [updateTable, setUpdateTable] = useState(false);
+
+  const tableResponse = useAxios(
+    {
+      method: "get",
+      endpoint: `${table}`,
+    },
+    [modifyPhrase, selectedPhraseId, updateTable]
+  );
 
   return (
     <div className={style.tablesDB}>
-      <div className={style.linkContainer}>
-        <Link to="/admin" className={style.linkReturnBtn}>
-          Return
-        </Link>
-      </div>
-      <section className={style.tableBody}>
+      {modifyPhrase && (
+        <ModifyPhrase
+          selectedPhraseId={selectedPhraseId}
+          setModifyPhrase={setModifyPhrase}
+          updateTable={updateTable}
+          setUpdateTable={setUpdateTable}
+        />
+      )}
+      {!modifyPhrase && (
+        <div className={style.linkContainer}>
+          <Link to="/admin" className={style.linkReturnBtn}>
+            Return
+          </Link>
+        </div>
+      )}
+      <section className={style.sectionTable}>
         <table className={style.table}>
           <thead>
             {table === "phrases" && (
@@ -26,6 +45,7 @@ export default function TablesDB() {
                 <th>Title</th>
                 <th>Author</th>
                 <th>Likes</th>
+                <th>Events</th>
               </tr>
             )}
             {table === "events" && (
@@ -39,7 +59,7 @@ export default function TablesDB() {
                 <th>Known Name</th>
                 <th>First Name</th>
                 <th>Last Name</th>
-                <th>Period Id</th>
+                <th>Period</th>
                 <th>Philo current</th>
                 <th>Born</th>
                 <th>Dead</th>
@@ -64,10 +84,17 @@ export default function TablesDB() {
             {table === "phrases" &&
               tableResponse &&
               tableResponse.map((item) => (
-                <tr key={item.id}>
+                <tr
+                  key={item.id}
+                  onClick={() => {
+                    setModifyPhrase(true);
+                    setSelectedPhraseId(item.id);
+                  }}
+                >
                   <td>{item.phrase}</td>
                   <td>{item.author}</td>
                   <td>{item.likes}</td>
+                  <td>{item.event_title}</td>
                 </tr>
               ))}
             {table === "events" &&
@@ -82,13 +109,14 @@ export default function TablesDB() {
               tableResponse &&
               tableResponse.map((item) => (
                 <tr key={item.id}>
-                  <td>{item.known_name} hello</td>
+                  <td>{item.known_name}</td>
                   <td>{item.firstname}</td>
                   <td>{item.lastname}</td>
-                  <td>{item.period_id}</td>
+                  <td>{item.period_title}</td>
                   <td>{item.philo_current}</td>
                   <td>{item.born_date}</td>
                   <td>{item.dead_date}</td>
+                  <td>{item.era}</td>
                 </tr>
               ))}
             {table === "categories" &&
@@ -111,11 +139,13 @@ export default function TablesDB() {
           </tbody>
         </table>
       </section>
-      <div className={style.linkContainer}>
-        <Link to="/admin" className={style.linkReturnBtn}>
-          Return
-        </Link>
-      </div>
+      {!modifyPhrase && (
+        <div className={style.linkContainer}>
+          <Link to="/admin" className={style.linkReturnBtn}>
+            Return
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
