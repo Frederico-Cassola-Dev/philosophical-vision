d6d2427bfe859adcffcf2161656e80286ab87a4d
Your Name <your@email.com>
2023-11-04 13:43:29 +0100

    Trying to remove lint jsx-a11y/label-has-associated-control error on github

--- frontend/src/components/ModifyPhrase/index.jsx
+++ frontend/src/components/ModifyPhrase/index.jsx
@@ -129,7 +129,7 @@ export default function ModifyPhrase({
     <div className={style.modal}>
       <h2>Phrase to modifiée</h2>
       <form className={style.phraseForm} onSubmit={handleSubmitForm}>
-        <label>
+        <label htmlFor="phrase">
           <textarea
             type="text"
             id="phrase"
