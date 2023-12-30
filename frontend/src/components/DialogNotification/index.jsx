99395d9e3c1f7ca9a9725525e0bc4c74e67d353e
Frederico-Cassola-Dev <cfcassola@gmail.com>
2023-12-30 14:00:07 +0100

    Add DialogNotification Component

---
+++ frontend/src/components/DialogNotification/index.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+import style from "./dialogNotification.module.scss";
+
+export default function DialogNotification({ dialogContent, setIsDialogOpen }) {
+  return (
+    <div className={style.dialogOverlay}>
+      <dialog open className={style.customDialog}>
+        <p className={style.dialogContent}>{dialogContent}</p>
+        <button
+          type="button"
+          className={style.dialogCloseButton}
+          onClick={() => setIsDialogOpen(false)}
+        >
+          Retorner
+        </button>
+      </dialog>
+    </div>
+  );
+}
+
+DialogNotification.propTypes = {
+  dialogContent: PropTypes.string.isRequired,
+  setIsDialogOpen: PropTypes.func.isRequired,
+};
