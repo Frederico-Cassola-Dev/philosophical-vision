f5428202110cba3e957a21feb97b76dc47648180
Frederico-Cassola-Dev <cfcassola@gmail.com>
2023-12-31 12:14:24 +0100

    Add table philo_currents

--- backend/src/models/AuthorManager.js
+++ backend/src/models/AuthorManager.js
@@ -33,8 +33,9 @@ class UserManager extends AbstractManager {
 
   async readAll() {
     const [rows] = await this.database.query(
-      `select a.id, known_name, first_name, last_name, p.title period_title, philo_current, born_date, dead_date, era from ${this.table} a
+      `select a.id, known_name, first_name, last_name, p.title period_title, philo_current_id, born_date, dead_date, era from ${this.table} a
       inner join periods p on p.id = a.period_id
+      inner join philo_currents pc on pc.id = a.philo_current_id
       `
     );
     return rows;
