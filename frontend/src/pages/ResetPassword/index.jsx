41bfb88e903cd51607114a63579b274f086bc1a7
Frederico-Cassola-Dev <cfcassola@gmail.com>
2024-01-13 13:23:00 +0100

    Add style to ResetPassword component

--- frontend/src/pages/ResetPassword/index.jsx
+++ frontend/src/pages/ResetPassword/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-// import { useParams } from "react-router-dom";
+
+import style from "./resetPassword.module.scss";
 
 export default function ResetPassword() {
   const [token, setToken] = useState("");
@@ -19,22 +20,80 @@ export default function ResetPassword() {
   };
 
   return (
-    <div>
-      <h2>Reset Password</h2>
-      <form onSubmit={handleResetPassword}>
-        <input
-          type="text"
-          placeholder="Token"
-          value={token}
-          onChange={(e) => setToken(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="New Password"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-        />
-        <button type="submit">Reset Password</button>
+    <div className={style.resetPassword}>
+      <h2 className={style.title}>Reset Password</h2>
+      <form className={style.formContainer} onSubmit={handleResetPassword}>
+        <label htmlFor="resetToken">
+          Votre token
+          <input
+            type="text"
+            id="resetToken"
+            name="resetToken"
+            placeholder="Insérez votre token"
+            value={token}
+            onChange={(e) => setToken(e.target.value)}
+          />
+        </label>
+        <label htmlFor="newPassword">
+          Nouvelle mot de passe
+          <input
+            type="password"
+            id="newPassword"
+            name="newPassword"
+            placeholder="Insérez votre nouvelle mot de passe"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
+        </label>
+        <label htmlFor="confirmPassword" className={style.labelConfirmPassword}>
+          Confirmation du mot de passe
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            // className={
+            //   newUserValidatorState.confirmPasswordError
+            //     ? style.errorInput
+            //     : style.inputConfirmPassword
+            // }
+            required
+            // value={newUserState.confirmPassword}
+            // onChange={(e) => {
+            //   dispatchForm({
+            //     type: SIGN_UP_UPDATE_CONFIRM_PASSWORD,
+            //     payload: { confirmPassword: e.target.value },
+            //   });
+            //   dispatchForm({
+            //     type: SIGN_UP_UPDATE_PASSWORDS_MATCH,
+            //     payload: {
+            //       passwordsMatch: !!(newUserState.password === e.target.value),
+            //     },
+            //   });
+            // }}
+            // onBlur={() => {
+            //   dispatchValidatorForm({
+            //     type: SIGN_UP_VALIDATE_CONFIRM_PASSWORD,
+            //     payload: newUserState,
+            //   });
+            //   dispatchValidatorForm({
+            //     type: SIGN_UP_VALIDATE_PASSWORD_MATCH,
+            //     payload: newUserState,
+            //   });
+            // }}
+          />
+          <span
+          // className={
+          //   newUserValidatorState.passwordsMatch
+          //     ? style.inputValidationMessage
+          //     : style.inputValidationHidden
+          // }
+          >
+            Le mot de passe doit égale
+          </span>
+        </label>
+        <div className={style.submitButtonContainer}>
+          <button type="submit">Réinitialiser votre mot de passe</button>
+        </div>
       </form>
     </div>
   );
