2606ae182dc17be213309d2e382016cf33b28fa4
Your Name <your@email.com>
2024-02-04 14:53:38 +0100

    Add resultat de la recherche aucun resulta trouve when no categiry selected only

--- frontend/src/components/SearchSelectModal/index.jsx
+++ frontend/src/components/SearchSelectModal/index.jsx
@@ -41,13 +41,14 @@ export default function SearchSelectModal({ state, dispatch }) {
         <h2 className={style.modalTitle}>Events</h2>
         <div className={style.listContainer}>
           <ul className={style.list}>
-            {eventsByTitleData.response?.length === 0 && (
-              <li className={style.listItems}>
-                <button type="button" disabled>
-                  "Aucun résulta trouvé"
-                </button>
-              </li>
-            )}
+            {eventsByTitleData.response?.length === 0 &&
+              state.categoryId === "" && (
+                <li className={style.listItems}>
+                  <button type="button" disabled>
+                    "Aucun résulta trouvé"
+                  </button>
+                </li>
+              )}
             {state.categoryId !== "" &&
               eventsByCategoryData?.response?.map((events) => (
                 <li className={style.listItems} key={events.id}>
